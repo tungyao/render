afd4c19bfc059d484e8a10552d3e9cfbbe1be4c0
tungyao <1922527051@qq.com>
2020-02-18 20:46:22 +0800

    fix all bug

--- README.md
+++ README.md
@@ -2,41 +2,26 @@
 前端快速渲染引擎
 ```html
 <div class='array' render-for="data">
-{item.name} {item.age}
-</div>
-
-<div class="root" render-if="false">
-    <h1>HELLO</h1>
+{{name}} {{age}}
 </div>
 
 <div class="two" render-obj="data">
-    <h1>{item.age}</h1>
-    <h1>{item.name}</h1>
+    <h1>{{age}}</h1>
+    <h1>{{name}}</h1>
 </div>
 ```
-### 老版本浏览器 请使用 `OldRender` 代替
 ```javascript
-// 渲染数组用这个
+// 渲染数组
+// render array
 NewRender(".array").for({data:[{name:"a",age:"b"},{name:"c",age:"d"}]})
+let X = new Render(".root");
+X.obj({data: {name: "AAAA", age: 123,ak:{yashua:"xxxx"}}});
 
-// 渲染object用这个
+
+// 渲染object
+// render object
 NewRender(".two").obj({data:{name:"a",age:"b"}})
-
-NewRender(".root").if(true);
-NewRender(".root").append(`<h1>HELLO WORLD</h1>`);
 ```
-## OR
-```javascript
-// 渲染数组用这个
-NewRender().setNode(".array").for({data:[{name:"a",age:"b"},{name:"c",age:"d"}]});
-
-// 渲染object用这个
-NewRender().setNode(".two").obj({data:{name:"a",age:"b"}})
-
-NewRender().setNode(".root").if(true);
-NewRender().setNode(".root").append(true);
-```
-
 
 
 
--- render.js
+++ render.js
@@ -61,7 +61,10 @@ class Render {
             }
             if (html[j] === "}" && html[j + 1] === "}") {
                 let nss = html.substring(a, j + 2);
-                let ns = nss.substring(2, nss.length-2);
+                let ns = nss.substring(2, nss.length - 2);
+                if (ns[0] === "{") {
+                    ns = ns.substring(1, nss.length);
+                }
                 let d = ns.split(".");
                 let key;
                 if (d.length >= 2) {
@@ -109,7 +112,6 @@ class Render {
             this.node = document.getElementsByClassName(node.substr(1, node.length - 1));
         }
         this.name = node;
-        this._check();
         return this;
     }
 
@@ -197,198 +199,6 @@ class Render {
     }
 }
 
-// 旧版浏览器使用 主要针对不兼容after的浏览器 把class删去
-var oRender = function oRender() {
-};
-oRender.prototype = {
-    _findPrefix: function (node, data) {
-        var c = node;
-        this._findAttr(c, data);
-        for (let i of c.childNodes) {
-            this._findAttr(i, data);
-            if (i.nodeType === 3) {
-                i = this._parseDom(i, data)
-            }
-            if (i.nodeType === 1) {
-                this._findChild(i.childNodes, data)
-            }
-        }
-        return c
-    },
-    _findAttr: function (node, data) {
-        if (typeof node.attributes === "undefined") {
-            return;
-        }
-        for (let i of node.attributes) {
-            this._parseDom(i, data)
-        }
-    },
-
-    _findChild: function (node, data) {
-        this._findAttr(node, data);
-        if (typeof node.length === "undefined") {
-            if (typeof node === "object" && node.nodeType === 1) {
-                this._parseDom(node, data);
-            }
-            return;
-        }
-        for (let i of node) {
-            this._findAttr(i, data);
-            if (i.nodeType === 3) {
-                this._parseDom(i, data)
-            } else if (i.nodeType === 1) {
-                this._findChild(i.childNodes, data)
-            }
-        }
-    },
-
-    _parseDom: function (node, data) {
-        var html = node.nodeValue;
-        if (node.nodeValue === null) {
-            html = node.innerHTML;
-        }
-        var a = 0;
-        for (var j in html) {
-            if (html[j] === "{") {
-                a = j;
-            }
-            if (html[j] === "}") {
-                var ns = html.substr(a, j - a + 1);
-                var nss = html.substr(a, j - a);
-                var d = nss.split(".");
-                var key;
-                if (d.length > 2) {
-                    key = this._fff(data[d[1]], d.pop());
-                } else {
-                    key = data[d[1]];
-                }
-                var value = html.replace(ns, key);
-                if (node.nodeValue === null) {
-                    node.innerHTML = value;
-                } else {
-                    node.nodeValue = value;
-                }
-                html = value;
-            }
-        }
-        return node;
-    },
-
-    _fff: function (object, key) {
-        if (typeof object === 'object') {
-            for (var i in object) {
-                if (typeof object[i] === 'object') {
-                    return this._fff(object[i], key);
-                }
-                if (key === i) {
-                    return object[key];
-                }
-            }
-        }
-        return object[key];
-    },
-
-    setNode: function (node) {
-        this.node = document.querySelectorAll(node);
-        if (typeof this.node !== "object") {
-            this.node = document.getElementsByClassName(node.substr(1, node.length - 1));
-        }
-        this.name = node;
-        this._check();
-        return this;
-    },
-
-    _check: function () {
-        for (var i of this.node) {
-            if (i.attributes.getNamedItem("render-if") !== null) {
-                if (i.attributes.getNamedItem("render-if").value === "false") {
-                    i.style.display = "none";
-                }
-            }
-        }
-    },
-
-    if(status) {
-        this.setNode(this.name);
-        for (var i of this.node) {
-            if (status) {
-                if (!i.attributes.getNamedItem("status")) {
-                    i.style.removeProperty("display")
-                }
-            } else {
-                i.style.display = "none";
-            }
-        }
-        return this;
-    },
-
-    _same(nodes, data, type) {
-        var node = nodes;
-        var dname;
-        if (type === 0) {
-            dname = nodes.attributes.getNamedItem("render-for").value;
-        } else if (type === 1) {
-            dname = nodes.attributes.getNamedItem("render-obj").value;
-        }
-        if (node.style.display === "none") {
-            node.style.removeProperty("display");
-            // node.attributes.removeNamedItem("render-for");
-        }
-        var n = node.nextSibling;
-        while (n) {
-            let nn = n;
-            n = n.nextSibling;
-            nn.remove();
-        }
-        if (node.attributes.getNamedItem("status") && this.status !== 0) {
-            node.attributes.removeNamedItem("status");
-        }
-        if (type === 0) {
-            for (let j of data[dname]) {
-                var parent = nodes.parentNode;
-                if (parent.lastChild === nodes) {
-                    parent.appendChild(this._findPrefix(node.cloneNode(true), j));
-                } else {
-                    parent.insertBefore(this._findPrefix(node.cloneNode(true), j), nodes.nextSibling)
-                }
-            }
-        } else if (type === 1) {
-            var parent = nodes.parentNode;
-            if (parent.lastChild === nodes) {
-                parent.appendChild(this._findPrefix(node.cloneNode(true), data[dname]));
-            } else {
-                parent.insertBefore(this._findPrefix(node.cloneNode(true), data[dname]), nodes.nextSibling)
-            }
-        }
-        nodes.style.display = "none";
-        nodes.attributes.setNamedItem(document.createAttribute("status"));
-    },
-
-    obj: function (data) {
-        let node = this.node[0];
-        let dname = nodes.attributes.getNamedItem("render-obj").value;
-    },
-
-    for: function (data) {
-        for (var i of this.node) {
-            this._same(i, data, 0);
-        }
-        this.status += 1;
-        return this;
-    },
-
-    append: function (node) {
-        for (var i of this.node) {
-            i.innerHTML = node
-        }
-        return this;
-    }
-};
-
-function OldRender() {
-    return new oRender();
-}
-
 function NewRender(node) {
-    return new Render().setNode(node);
+    return new Render(node);
 }
